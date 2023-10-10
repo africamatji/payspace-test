d0f50e51ec4841747d7c75205472c7210ec0e260
africam <africamatji@gmail.com>
2023-10-10 16:30:57 +0200

    pagination

--- js/data/Books.js
+++ js/data/Books.js
@@ -5,7 +5,4 @@ export const Books = [
     { title: 'X The Great Gatsby', author: 'D. Sccott Fitz', year: 1925 },
     { title: 'W To kill a mocking bird', author: 'Garper Lee', year: 1960 },
     { title: 'S Test book title here', author: 'Joh Doe', year: 1980 },
-    { title: 'T The Great Gatsby', author: 'X. Sccott Fitz', year: 1925 },
-    { title: 'R To kill a mocking bird', author: 'Z. Garper Lee', year: 1960 },
-    { title: 'X Test book title here', author: 'Joh Doe', year: 1980 },
 ];
