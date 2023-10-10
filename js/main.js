d39859d52ed90629381a0efd066c4a711ab46b21
africam <africamatji@gmail.com>
2023-10-10 12:19:37 +0200

    refactor

--- js/main.js
+++ js/main.js
@@ -6,31 +6,23 @@ const bookList = booksObj.bookList;
 
 booksObj.renderView(bookList); //render view with default list
 
-//Search event
+//Add input events
 document.getElementById('search-txt').addEventListener('keyup', (event) => {
     const data = booksObj.searchByTitleAuthor(event.target.value);
     booksObj.renderView(data);
 });
-
-// Title ascending
 document.getElementById('sort-title-asc').addEventListener('click', (event) => {
     const data = bookList.slice().sort(booksObj.sortByTitles);
     booksObj.renderView(data);
 });
-
-// Title descending
 document.getElementById('sort-title-desc').addEventListener('click', (event) => {
     const data = bookList.slice().sort((a, b) => booksObj.sortByTitles(b, a));
     booksObj.renderView(data);
 });
-
-// Author ascending
 document.getElementById('sort-author-asc').addEventListener('click', (event) => {
     const data = bookList.slice().sort(booksObj.sortByAuthors);
     booksObj.renderView(data);
 });
-
-// Author descending
 document.getElementById('sort-author-desc').addEventListener('click', (event) => {
     const data = bookList.slice().sort((a, b) => booksObj.sortByAuthors(b, a));
     booksObj.renderView(data);
