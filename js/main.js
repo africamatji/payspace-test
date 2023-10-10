91202e584882ee56ccfd85afbc57081ca8ead832
africam <africamatji@gmail.com>
2023-10-10 12:09:00 +0200

    filter, search works

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ import Books from "./books.js";
 import { View } from "./utility/View.js"; //utility class to abstract the view rendering functionality
 
 const booksObj = new Books();
-View.render(booksObj.bookList);
+const bookList = booksObj.bookList;
+View.render(bookList);
 
 //Search event
 document.getElementById('search-txt').addEventListener('keyup', (event) => {
@@ -13,24 +14,24 @@ document.getElementById('search-txt').addEventListener('keyup', (event) => {
 
 // Title ascending
 document.getElementById('sort-title-asc').addEventListener('click', (event) => {
-    const data = booksObj.bookList.slice().sort(booksObj.sortByTitles);
+    const data = bookList.slice().sort(booksObj.sortByTitles);
     View.render(data);
 });
 
 // Title descending
 document.getElementById('sort-title-desc').addEventListener('click', (event) => {
-    const data = booksObj.bookList.slice().sort((a, b) => booksObj.sortByTitles(b, a));
+    const data = bookList.slice().sort((a, b) => booksObj.sortByTitles(b, a));
     View.render(data);
 });
 
 // Author ascending
 document.getElementById('sort-author-asc').addEventListener('click', (event) => {
-    const data = booksObj.bookList.slice().sort(booksObj.sortByAuthors);
+    const data = bookList.slice().sort(booksObj.sortByAuthors);
     View.render(data);
 });
 
 // Author descending
 document.getElementById('sort-author-desc').addEventListener('click', (event) => {
-    const data = booksObj.bookList.slice().sort((a, b) => booksObj.sortByAuthors(b, a));
+    const data = bookList.slice().sort((a, b) => booksObj.sortByAuthors(b, a));
     View.render(data);
 });
